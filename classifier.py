af44d166328239ba64021032d9d7ffdd050cb80b
Or4ng3 <imor4ng3@gmail.com>
2026-04-29 17:24:10 +0800

    feat: classifier finished

--- classifier.py
+++ classifier.py
@@ -55,7 +55,8 @@ class GPT2SentimentClassifier(torch.nn.Module):
 
     ### TODO: Create any instance variables you need to classify the sentiment of BERT embeddings.
     ### YOUR CODE HERE
-    raise NotImplementedError
+    self.classifier = torch.nn.Linear(config.hidden_size, self.num_labels)
+    self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
 
 
   def forward(self, input_ids, attention_mask):
@@ -65,7 +66,11 @@ class GPT2SentimentClassifier(torch.nn.Module):
     ###       HINT: You should consider what is an appropriate return value given that
     ###       the training loop currently uses F.cross_entropy as the loss function.
     ### YOUR CODE HERE
-    raise NotImplementedError
+    output = self.gpt(input_ids, attention_mask)  # last_token: batch_size, hidden_dim
+    last_token = output["last_token"]
+    last_token = self.dropout(last_token)
+    logits = self.classifier(last_token)
+    return logits
 
 
 
